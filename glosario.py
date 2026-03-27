7a21cd4846a2663f2faffaf0e57f57725bcfd4d2
Ricardo Iraola <ricardoiraola12@gmail.com>
2026-03-27 01:22:32 +0000

    feat: agrega definicion de VirtualEnv

--- glosario.py
+++ glosario.py
@@ -29,26 +29,13 @@ def glosario_inicial():
     for termino, definicion in terminos.items():
         print(f"-> {termino}: {definicion}")
 
-
-# --- TAREA PARA EL ALUMNO ---
-# Instrucciones:
-# 1. Crea una rama 'feature-tu-termino'
-# 2. Crea una función nueva siguiendo el ejemplo de abajo
-# 3. Llámala dentro del bloque 'if __name__ == "__main__":'
-
-
-def agregar_definicion_alumno():
-    """
-    EJEMPLO: El alumno debe renombrar esta función y completar su término.
-    """
-    termino = "ORM"  # Reemplazar con el término asignado o elegido
-    definicion = "Object-Relational Mapping. Técnica para convertir datos entre sistemas incompatibles usando OOP."
+def glosario_virtualenv():
+    termino = "VENV"  # Reemplazar con el término asignado o elegido
+    definicion = "Virtual-Enviroments. Es una herramienta que permite crear entornos aislados de Python para gestionar dependencias de manera independiente."
     print(f"[NUEVO] {termino}: {definicion}")
 
 
 if __name__ == "__main__":
     mostrar_bienvenida()
     glosario_inicial()
-
-    # El alumno debe agregar la llamada a su función aquí:
-    # agregar_definicion_alumno()
+    glosario_virtualenv()
